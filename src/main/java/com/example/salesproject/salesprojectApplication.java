c68fd08a50906a9a4f8e04d46825675641844cf6
“sengeramit78” <sengeramit78@gmail.com>
2024-02-10 10:40:15 +0530

    alpha

---
+++ src/main/java/com/example/salesproject/salesprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.salesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class salesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(salesprojectApplication.class, args);
+		System.out.println("Working");
+	}
+
+}
